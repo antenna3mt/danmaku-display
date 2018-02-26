c44af8d38a85a562fa2a9d86986234a71f1edde2
Yi <antenna3mt@gmail.com>
2018-02-26 03:23:53 -0500

    part

---
+++ src/main/java/danmaku/display/Window.java
@@ -0,0 +1,10 @@
+package danmaku.display;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Window extends Application {
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
